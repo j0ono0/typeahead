98b0c5f2342b3de4c4fa5e9b0d10547d8486eb81
John Newall <john@johnnewall.com>
2025-04-15 15:26:41 +0930

    update input selector

--- typeahead.js
+++ typeahead.js
@@ -14,40 +14,40 @@ class Typeahead {
 
     inputListener(evt) {
         clearTimeout(this.timer);
-        this.timer = setTimeout(this.tmdb_film_search.bind(this), this.delay);
+        this.timer = setTimeout(this.create_suggestion_list.bind(this), this.delay);
     }
 
-    tmdb_film_search() {
-        const search_url = `https://api.themoviedb.org/3/search/movie?query=${this.input_title.value}&include_adult=false&language=en-US&page=1`;
+    create_suggestion_list(){
+        this.tmdb_film_search(this.input_title.value, this.tmdb_token)
+        .then(json=> {
+            this.clear_title_suggestion_list();
+            if(json.total_results > 0){
+                const elem = this.build_movie_suggestion_list(json);
+                this.component.append(elem);
+            }
+        })
+    }
+    
+    tmdb_film_search(title_search_text, tmdb_token) {
+        const search_url = `https://api.themoviedb.org/3/search/movie?query=${title_search_text}&include_adult=false&language=en-US&page=1`;
         const options = {
             method: 'GET',
             headers: {
                 accept: 'application/json',
-                Authorization: `Bearer ${this.tmdb_token}`
+                Authorization: `Bearer ${tmdb_token}`
             }
         };
-        fetch(search_url, options)
+        return fetch(search_url, options)
         .then(res => res.json())
-        .then(json => {
-            this.insert_title_suggestions(json)
-        })
         .catch(err => console.error(err));
     }
-
+    
     clear_title_suggestion_list(){
         // Remove any existing title_suggestion_lists
         document.querySelectorAll("#title_suggestion_list").forEach(elem=>elem.remove());
     }
-
-    insert_title_suggestions(json){
-
-        this.clear_title_suggestion_list();
-        
-        // Exit if no result to list
-        if(json.total_results == 0){
-            return;
-        }
-
+    
+    build_movie_suggestion_list(json){
         let ul = document.createElement("ul");
         ul.id = "title_suggestion_list"
         json.results.forEach(movie_details=>{
@@ -57,17 +57,14 @@ class Typeahead {
             li.appendChild(anchor)
             anchor.href = `#${movie_details.id}`;
             anchor.innerHTML = `${movie_details.title} (${movie_details.release_date.split("-")[0]})`
-
-            anchor.addEventListener('click', this.tmdb_film_data.bind(this, movie_details))
+            
+            anchor.addEventListener('click', this.populate_input_with_tmdb_data.bind(this, movie_details))
         })
-        // Add new list
-        this.component.append(ul);
+        return ul
     }
     
-    tmdb_film_data(movie_details) {
-
+    populate_input_with_tmdb_data(movie_details) {
         this.clear_title_suggestion_list();
-        
         const options = {
             method: 'GET',
             headers: {
@@ -75,13 +72,11 @@ class Typeahead {
                 Authorization: `Bearer ${this.tmdb_token}`
             }
         };
-
         const credits_url = `https://api.themoviedb.org/3/movie/${movie_details.id}/credits?language=en-US`;
         fetch(credits_url, options)
         .then(res => res.json())
         .then(res => {
             let directors = res.crew.filter(person => person.job == "Director")
-
             this.input_title.value = movie_details.title;
             this.input_year.value = movie_details.release_date.split("-")[0];
             this.input_director.value = directors[0].name;
@@ -90,10 +85,3 @@ class Typeahead {
     }
     
 }
-
-
-
-
-
-///////////////////////////////////////////////////////////////////////////////////////////////
-
